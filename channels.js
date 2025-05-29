f8fa6bc90d8ef4dcd463213058220b50181769af
RN-Akash <akash@iquincesoft.com>
2025-05-29 15:08:39 +0530

    initial ui

--- channels.js
+++
@@ -1,8 +0,0 @@
-export const channels = [
-  {
-    "title": "title of channel",
-    "group": "Music",
-    "logo": "https://www.google.com/logo.png",
-    "url": "http://streamurl/putherer.m3u8"
-  },
-]
