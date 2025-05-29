ee5fc465d991dee1ba796e51768439e1ca4b795e
RN-Akash <akash@iquincesoft.com>
2025-05-29 15:11:50 +0530

    initial ui

---
+++ channel.js
@@ -0,0 +1,30 @@
+export const channels = [
+    {
+        "id": 1,
+        "title": "7S Music (576p) [Not 24/7]",
+        "group": "Music",
+        "logo": "https://i.imgur.com/zDiIhdN.png",
+        "url": "http://103.199.161.254/Content/7smusic/Live/Channel(7smusic)/index.m3u8"
+    },
+    {
+        "id": 2,
+        "title": "9x Jhakaas",
+        "group": "Music",
+        "logo": "https://i.imgur.com/aZ0f85I.png",
+        "url": "https://amg01281-9xmediapvtltd-9xjhakaas-samsungin-ci2cs.amagi.tv/playlist/amg01281-9xmediapvtltd-9xjhakaas-samsungin/playlist.m3u8"
+    },
+    {
+        "id": 3,
+        "title": "9X Tashan",
+        "group": "Music",
+        "logo": "https://i.imgur.com/MMR1RMF.png",
+        "url": "https://amg01281-9xmediapvtltd-9xtashan-samsungin-xz1sd.amagi.tv/playlist/amg01281-9xmediapvtltd-9xtashan-samsungin/playlist.m3u8"
+    },
+    {
+        "id": 4,
+        "title": "9XM (576p)",
+        "group": "Music",
+        "logo": "https://i.imgur.com/F17QtN2.png",
+        "url": "https://d35j504z0x2vu2.cloudfront.net/v1/manifest/0bc8e8376bd8417a1b6761138aa41c26c7309312/9xm/23886666-8fc5-470f-aab1-bd637ed607b1/3.m3u8"
+    },
+]
